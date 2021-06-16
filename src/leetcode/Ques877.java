fc085151dd661c001252809d5e1af10788ea503a
ming <984396692@qq.com>
2021-06-16 16:24:24 +0800

    0615

---
+++ src/leetcode/Ques877.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+
+public class Ques877 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    //看了答案才会的
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
