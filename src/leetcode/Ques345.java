1f8b640c10ba5e03e37679cfbdcdd4879dbe5d10
ming <984396692@qq.com>
2021-08-19 17:09:50 +0800

    0819-ming

---
+++ src/leetcode/Ques345.java
@@ -0,0 +1,85 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Ques345 {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public String reverseVowels(String s) {
+		if(s==null || s.length() == 0 || s.length() == 1){
+			return s;
+		}
+		HashSet<Character> set = new HashSet<>();
+		set.add('a');
+		set.add('e');
+		set.add('i');
+		set.add('o');
+		set.add('u');
+		set.add('A');
+		set.add('E');
+		set.add('I');
+		set.add('O');
+		set.add('U');
+		char[] chars = s.toCharArray();
+		LinkedList<Character> charList = new LinkedList<>();
+		LinkedList<Integer> positionList = new LinkedList<>();
+		for (int i = 0; i < s.length(); i++) {
+			char cur = s.charAt(i);
+			if(set.contains(cur)){
+				charList.addFirst(cur);
+				positionList.addLast(i);
+			} else {
+				chars[i] = cur;
+			}
+		}
+		for (int i = 0; i < charList.size(); i++) {
+			chars[positionList.get(i)] = charList.get(i);
+		}
+		return new String(chars);
+	}
+
+	public String reverseVowels2(String s) {
+		if(s==null || s.length() == 0 || s.length() == 1){
+			return s;
+		}
+		HashSet<Character> set = new HashSet<>();
+		set.add('a');
+		set.add('e');
+		set.add('i');
+		set.add('o');
+		set.add('u');
+		set.add('A');
+		set.add('E');
+		set.add('I');
+		set.add('O');
+		set.add('U');
+		char[] chars = new char[s.length()];
+		int left = 0;
+		int right = chars.length-1;
+		while (left<right){
+			while (left<right && !set.contains(chars[left])){
+				left++;
+			}
+			if(left >= right){
+				break;
+			}
+			while (left<right && !set.contains(chars[right])){
+				right--;
+			}
+			if(left >= right){
+				break;
+			}
+			char temp = chars[left];
+			chars[left] = chars[right];
+			chars[right] = temp;
+			left++;
+			right--;
+		}
+
+		return new String(chars);
+	}
+}
